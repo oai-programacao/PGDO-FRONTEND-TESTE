f29f4a6c554b660fa8775dfacaef69be630b9332
Gabriela <gabiviana.cunha@gmail.com>
2025-12-19 08:26:57 -0300

    feat: add s in http

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "http://sandbox.oai.com.br/api",
+  apiUrl: "https://sandbox.oai.com.br/api",
   wsUrl: "wss://sandbox.oai.com.br/api/ws-connect",
   googleMapsApiKey: "AIzaSyCmavFR3WM6LhreBeGEBSKr5nKBAHcWVms",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8080/api",
+  apiUrl: "https://localhost:8080/api",
   wsUrl: "ws://localhost:8080/api/ws-connect",
   googleMapsApiKey: "AIzaSyCmavFR3WM6LhreBeGEBSKr5nKBAHcWVms",
 };
