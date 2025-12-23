95d24060a6b6a18c6642f09f5de1099c0cb74c3a
Gabriel Santos <gabrielmath14@outlook.com>
2025-12-23 10:20:02 -0300

    add search contract

--- src/app/features/service-orders/services/service-order.service.ts
+++ src/app/features/service-orders/services/service-order.service.ts
@@ -29,7 +29,7 @@ export class ServiceOrderService {
 
   getContractDetails(contractNumber: number): Observable<any> {
     return this.http.post<any>(
-      `${environment.apiUrl}/rbx/consultar-contratos?filtro=Numero=${contractNumber}`,
+      `${environment.apiUrl}/contract/rbx/consultar-contratos?filtro=Numero=${contractNumber}`,
       null
     );
   }
