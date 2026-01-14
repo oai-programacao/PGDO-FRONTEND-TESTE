1874f32fc68a11167a73a40ff00ca65038329025
Gabriel Santos <gabrielmath14@outlook.com>
2026-01-14 10:54:57 -0300

    .

--- src/app/features/service-orders/pages/admin-service-orders/admin-service-orders.component.ts
+++ src/app/features/service-orders/pages/admin-service-orders/admin-service-orders.component.ts
@@ -424,6 +424,8 @@ export class AdminServiceOrdersComponent implements OnInit, OnDestroy {
       return;
     }
 
+    
+
     const dto: UpdateServiceOrderDto = {
       scheduleDate: formGroup.get("scheduleDate")?.value || null,
       period: formGroup.get("period")?.value || null,
